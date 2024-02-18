f3c6f1697aeb0af1a703cd56b1ae994b043d86f5
Jan Hohenheim <jan@hohenheim.ch>
2024-02-18 23:56:37 +0100

    Move assets into plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use bevy::asset::load_internal_asset;
+use bevy::render::render_asset::RenderAssetUsages;
+use bevy::render::texture::{CompressedImageFormats, ImageSampler, ImageType};
 use bevy::scene::SceneInstance;
 use bevy::{
     pbr::{ExtendedMaterial, MaterialExtension},
@@ -14,6 +17,32 @@ pub struct WindWakerShaderPlugin;
 
 impl Plugin for WindWakerShaderPlugin {
     fn build(&self, app: &mut App) {
+        load_internal_asset!(
+            app,
+            SHADER_HANDLE,
+            "assets/toon_shader.wgsl",
+            Shader::from_wgsl
+        );
+
+        let buffer = include_bytes!("assets/ZAtoon.png");
+        let extension = ImageType::Extension("png");
+        let compression = CompressedImageFormats::default();
+        let is_srgb = false;
+        let sampler = ImageSampler::default();
+        let render_asset_usages = RenderAssetUsages::RENDER_WORLD;
+        let img = Image::from_buffer(
+            buffer,
+            extension,
+            compression,
+            is_srgb,
+            sampler,
+            render_asset_usages,
+        )
+        .unwrap();
+        app.world
+            .resource_mut::<Assets<Image>>()
+            .insert(TEXTURE_HANDLE, img);
+
         app.add_plugins(MaterialPlugin::<
             ExtendedMaterial<StandardMaterial, WindWakerShaderMaterial>,
         >::default())
@@ -31,20 +60,32 @@ pub type Material = ExtendedMaterial<StandardMaterial, WindWakerShaderMaterial>;
 pub struct WindWakerShaderMaterial {
     #[texture(100)]
     #[sampler(101)]
-    mask: Option<Handle<Image>>,
+    mask: Handle<Image>,
     #[uniform(102)]
     pub config: WindWakerShader,
 }
 
 impl MaterialExtension for WindWakerShaderMaterial {
     fn fragment_shader() -> ShaderRef {
-        "shaders/toon_shader.wgsl".into()
+        SHADER_HANDLE.into()
     }
 
     fn deferred_fragment_shader() -> ShaderRef {
-        "shaders/toon_shader.wgsl".into()
+        SHADER_HANDLE.into()
     }
 }
+
+impl From<WindWakerShader> for WindWakerShaderMaterial {
+    fn from(config: WindWakerShader) -> Self {
+        Self {
+            mask: TEXTURE_HANDLE.clone(),
+            config,
+        }
+    }
+}
+
+const SHADER_HANDLE: Handle<Shader> = Handle::weak_from_u128(4_326_875_112_478_868_553);
+const TEXTURE_HANDLE: Handle<Image> = Handle::weak_from_u128(8_324_623_845_322_188_856);
 
 #[derive(Asset, AsBindGroup, Reflect, Debug, Clone, Component, ShaderType)]
 pub struct WindWakerShader {
@@ -74,7 +115,6 @@ fn customize_scene_materials(
     scene_manager: Res<SceneSpawner>,
     mut materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, WindWakerShaderMaterial>>>,
     mut cmds: Commands,
-    asset_server: Res<AssetServer>,
 ) {
     for (entity, instance, config) in unloaded_instances.iter() {
         if let Some(instance) = instance {
@@ -89,10 +129,7 @@ fn customize_scene_materials(
                 };
                 let toon_material = materials.add(ExtendedMaterial {
                     base: material.clone(),
-                    extension: WindWakerShaderMaterial {
-                        mask: Some(asset_server.load("textures/ZAtoon.png")),
-                        config: config.clone(),
-                    },
+                    extension: WindWakerShaderMaterial::from(config.clone()),
                 });
                 cmds.entity(entity)
                     .insert(toon_material)
@@ -110,7 +147,6 @@ fn customize_standard_materials(
     mut materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, WindWakerShaderMaterial>>>,
     pbr_materials: Res<Assets<StandardMaterial>>,
     mut cmds: Commands,
-    asset_server: Res<AssetServer>,
 ) {
     for (entity, material_handle, config) in with_material.iter() {
         let Some(material) = pbr_materials.get(material_handle) else {
@@ -118,10 +154,7 @@ fn customize_standard_materials(
         };
         let toon_material = materials.add(ExtendedMaterial {
             base: material.clone(),
-            extension: WindWakerShaderMaterial {
-                mask: Some(asset_server.load("textures/ZAtoon.png")),
-                config: config.clone(),
-            },
+            extension: WindWakerShaderMaterial::from(config.clone()),
         });
         cmds.entity(entity)
             .insert(toon_material)
