a756ef908a7582d906cba0173578771eac9971da
Jan Hohenheim <jan@hohenheim.ch>
2024-02-19 02:31:37 +0100

    Make plugin non exhaustive

--- src/plugin.rs
+++ src/plugin.rs
@@ -10,8 +10,8 @@ use bevy::render::render_asset::RenderAssetUsages;
 use bevy::render::texture::{CompressedImageFormats, ImageSampler, ImageType};
 
 /// Plugin for the Wind Waker shader.
-#[derive(Debug, Default, Clone, Copy)]
 #[non_exhaustive]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct WindWakerShaderPlugin;
 
 impl Plugin for WindWakerShaderPlugin {
